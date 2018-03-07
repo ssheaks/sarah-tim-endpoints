4b81afa60d8ece167ef2eebe64f6e392dd784107
Timothy Song <tsong0220@gmail.com>
2018-03-07 12:30:38 -0700

    added delete endpoint

--- server.js
+++ server.js
@@ -52,6 +52,12 @@ app.post('/shopping-list', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
+app.delete('/shopping-list/:id', (req, res) => {
+  ShoppingList.delete(req.params.id);
+  console.log(`Deleted shopping list item \`${req.params.id}\``);
+  res.status(204).end();
+});
+
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
 });
@@ -69,6 +75,12 @@ app.post('/recipes', jsonParser, (req, res) => {
   }
   const item = Recipes.create(req.body.name, req.body.ingredients);
   res.status(201).json(item);
+});
+
+app.delete('/recipes/:id', (req, res) => {
+  Recipes.delete(req.params.id);
+  console.log(`Deleted recipes \`${req.params.id}\``);
+  res.status(204).end();
 });
 
 app.listen(process.env.PORT || 8080, () => {
